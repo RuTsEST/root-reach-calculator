87e7478f391222fda9922d783c8d2c3432df06b5
Ruudi <ruudivinter@gmail.com>
2021-09-29 20:43:15 +0300

    Player count button toggling

--- src/components/PlayerCountButtons.js
+++ src/components/PlayerCountButtons.js
@@ -1,12 +1,22 @@
-import {Button, ButtonGroup} from "@mui/material";
+import * as React from 'react';
+import {Stack, ToggleButton, ToggleButtonGroup} from "@mui/material";
 
 export const PlayerCountButtons = () => {
+    const [playerCount, setPlayerCount] = React.useState(4)
+
+    const handlePlayerCount = (event, newPlayerCount) => {
+        setPlayerCount(newPlayerCount);
+    }
+
     return (
-        <ButtonGroup>
-            <Button>3</Button>
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-        </ButtonGroup>
+        <Stack justifyContent="center">
+            <h4>Players</h4>
+            <ToggleButtonGroup exclusive value={playerCount} onChange={handlePlayerCount}>
+                <ToggleButton value={3}>3</ToggleButton>
+                <ToggleButton value={4}>4</ToggleButton>
+                <ToggleButton value={5}>5</ToggleButton>
+                <ToggleButton value={6}>6</ToggleButton>
+            </ToggleButtonGroup>
+        </Stack>
     )
 }
