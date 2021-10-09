709cef966d423704fde38d71906ba9665d1c289c
Ruudi <ruudivinter@gmail.com>
2021-10-09 13:27:27 +0300

    Add cypress types and eslint plugin

--- cypress/integration/App.e2e.js
+++ cypress/integration/App.e2e.js
@@ -1,15 +1,15 @@
 describe('App E2E', () => {
     it('should pass happy path', () => {
         cy.visit('/');
-        cy.get('[data-cy=reach-indicator]').should('have.text', '0/21+')
+        cy.get('[data-cy=reach-indicator]').should('have.text', '0/21+');
         cy.get('[data-cy=marquise]').click();
-        cy.get('[data-cy=reach-indicator]').should('have.text', '10/21+')
+        cy.get('[data-cy=reach-indicator]').should('have.text', '10/21+');
         cy.get('[data-cy=alliance]').click();
-        cy.get('[data-cy=reach-indicator]').should('have.text', '13/21+')
+        cy.get('[data-cy=reach-indicator]').should('have.text', '13/21+');
         cy.get('[data-cy=cult]').click();
-        cy.get('[data-cy=riverfolk]').should('be.disabled')
+        cy.get('[data-cy=riverfolk]').should('be.disabled');
         cy.get('[data-cy=duchy]').click();
-        cy.get('[data-cy=reach-indicator]').should('have.text', '23/21+')
-        cy.get('[data-cy=eyrie]').should('be.disabled')
+        cy.get('[data-cy=reach-indicator]').should('have.text', '23/21+');
+        cy.get('[data-cy=eyrie]').should('be.disabled');
     });
 });
