1fe85051ec90a1325a2b414e91ad3c7f25192add
Ruudi <ruudivinter@gmail.com>
2022-03-13 17:16:03 +0200

    Add background colors to buttons for better visibility

--- src/components/ChooseFactionButton.js
+++ src/components/ChooseFactionButton.js
@@ -1,19 +1,29 @@
 import {Avatar, Badge, Fab} from "@mui/material";
 import * as React from "react";
-import {IS_NOT_AVAILABLE} from "../constants";
+import {IS_AVAILABLE, IS_BANNED, IS_NOT_AVAILABLE, IS_PICKED} from "../constants";
 import {FactionStatusOverlay} from "./FactionStatusOverlay";
 
+
+const getFactionButtonColor = (faction) => {
+    if (faction.status === IS_PICKED) {
+        return "#4da251"
+    }
+    if (faction.status === IS_BANNED) {
+        return "#b66969"
+    }
+}
 
 export const ChooseFactionButton = ({faction, handleFactionClick}) => {
     const widthAndHeight = faction.status === IS_NOT_AVAILABLE ? "4em" : "7em"
     const badgeTopAndRight = faction.status === IS_NOT_AVAILABLE ? "0.5em" : "1em"
     const badgeFontSize = faction.status === IS_NOT_AVAILABLE ? "small" : "large";
     const opacity = faction.status === IS_NOT_AVAILABLE ? "50%" : "100%"
+    const fabColor = getFactionButtonColor(faction);
 
     return (
         <Fab onClick={() => handleFactionClick(faction.name)}
              value={faction.reach}
-             sx={{width: widthAndHeight, height: widthAndHeight, opacity: opacity}}
+             sx={{width: widthAndHeight, height: widthAndHeight, opacity: opacity, backgroundColor: fabColor}}
              disabled={faction.status === IS_NOT_AVAILABLE}
              data-cy={faction.name}
         >
