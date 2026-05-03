2928e36366438d367239b62ff9316687e7a33488
Ruudi Vinter <ruudi.vinter@pipedrive.com>
2026-05-03 15:48:57 +0300

    test: replace broken scaffold test with meaningful App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('reach indicator renders with initial value', () => {
+  const { container } = render(<App />);
+  const indicator = container.querySelector('[data-cy="reach-indicator"]');
+  expect(indicator).toBeInTheDocument();
+  expect(indicator).toHaveTextContent('0/21+');
+});
+
+test('pick mode: clicking a faction adds its reach to the total', () => {
+  const { container } = render(<App />);
+  fireEvent.click(container.querySelector('[data-cy="marquise"]'));
+  expect(container.querySelector('[data-cy="reach-indicator"]')).toHaveTextContent('10/21+');
+});
+
+test('ban mode: clicking a faction bans it without adding reach', () => {
+  const { container } = render(<App />);
+  fireEvent.click(screen.getByRole('button', { name: /disable factions/i }));
+  fireEvent.click(container.querySelector('[data-cy="marquise"]'));
+  expect(container.querySelector('[data-cy="reach-indicator"]')).toHaveTextContent('0/21+');
+});
+
+test('selecting 1 clockwork 1 player sets required reach to 8', () => {
+  const { container } = render(<App />);
+  // div.container order: [0] Players, [1] 1 Clockwork, [2] 2 Clockworks
+  const oneClockworkSection = container.querySelectorAll('div.container')[1];
+  fireEvent.click(within(oneClockworkSection).getByText('1'));
+  expect(container.querySelector('[data-cy="reach-indicator"]')).toHaveTextContent('0/8+');
 });
